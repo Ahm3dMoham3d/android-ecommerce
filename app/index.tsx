2717aa21864b45f60fed8741296a96c2b7f06343
Ahmed Mohamed <116577902+Ahm3dMoham3d@users.noreply.github.com>
2024-11-03 16:51:33 +0200

    Start

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "@react-navigation/native";
+export default function index() {
+  const { colors } = useTheme();
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ color: colors.text }}>Hello world</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
