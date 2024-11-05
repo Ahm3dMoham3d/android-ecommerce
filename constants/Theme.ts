522cb40e6ff3c9f3cad337e02318cb1f24c79f8f
Ahmed Mohamed <116577902+Ahm3dMoham3d@users.noreply.github.com>
2024-11-05 03:12:20 +0200

    test

---
+++ constants/Theme.ts
@@ -0,0 +1,11 @@
+export const AppTheme = {
+  dark: false,
+  colors: {
+    primary: "#0381fe",
+    background: "#F2F2F7",
+    card: "#FFFFFF",
+    text: "#1C1C1E",
+    border: "#D1D1D6",
+    notification: "#34C759",
+  },
+};
